fbf176a66e16327e476124a1680c76a04bf20a45
Michael Liao <michael.liao@intel.com>
2024-02-20 12:26:24 -0500

    [elfextract] Add '-l' option to list all functions defined

--- elfextract/main.py
+++ elfextract/main.py
@@ -9,7 +9,7 @@ from elftools.elf.sections import SymbolTableSection
 
 def enum_all_funcs(filename, verbose=False):
     funcs = []
-    with open(filename, 'rb') as file:
+    with open(filename, "rb") as file:
         elffile = ELFFile(file)
         symbol_tables = [
             (idx, s)
@@ -18,17 +18,17 @@ def enum_all_funcs(filename, verbose=False):
         ]
         for idx, s in symbol_tables:
             for nsym, symbol in enumerate(s.iter_symbols()):
-                if symbol['st_info']['type'] != 'STT_FUNC':
+                if symbol["st_info"]["type"] != "STT_FUNC":
                     continue
                 if verbose:
-                    print('Found a function symbol', symbol.name)
+                    print("Found a function symbol", symbol.name)
                 funcs.append(symbol.name)
     return funcs
 
 
 def extract_func(filename, func, verbose=False):
     base, ext = os.path.splitext(filename)
-    with open(filename, 'rb') as file:
+    with open(filename, "rb") as file:
         elffile = ELFFile(file)
         symbol_tables = [
             (idx, s)
@@ -40,16 +40,16 @@ def extract_func(filename, func, verbose=False):
             if not syms:
                 continue
             symbol = syms[0]
-            shndx = symbol['st_shndx']
-            offset = symbol['st_value']
-            size = symbol['st_size']
+            shndx = symbol["st_shndx"]
+            offset = symbol["st_value"]
+            size = symbol["st_size"]
             text = elffile.get_section(shndx)
             if verbose:
-                print('Extracting', func)
-            with open(base + '.' + func + '.bin', 'wb') as f:
+                print("Extracting", func)
+            with open(base + "." + func + ".bin", "wb") as f:
                 f.write(text.data()[offset : offset + size])
             return
-        print(func, 'is not found')
+        print(func, "is not found")
 
 
 class ListAction(Action):
@@ -57,7 +57,7 @@ class ListAction(Action):
 
     def __call__(self, parser, namespace, values, option_string=None):
         if isinstance(values, list):
-            values = values.split(',')
+            values = values.split(",")
         v = getattr(namespace, self.dest)
         if v is not None:
             v.extend(values)
@@ -67,26 +67,38 @@ class ListAction(Action):
 
 
 def main():
-    parser = ArgumentParser(description='')
+    parser = ArgumentParser(description="")
     parser.add_argument(
-        '-f',
-        '--func',
-        dest='funcs',
+        "-f",
+        "--func",
+        dest="funcs",
         action=ListAction,
-        help='Functions to be extracted, separated by comma',
+        help="Functions to be extracted, separated by comma",
     )
     parser.add_argument(
-        '-v',
-        '--verbose',
-        dest='verbose',
-        action='store_true',
-        help='Enable verbose output',
+        "-l",
+        "--list",
+        dest="list_only",
+        action="store_true",
+        help="List symbols of functions defined",
     )
-    parser.add_argument('file', nargs='+', help='ELF file to extract')
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="Enable verbose output",
+    )
+    parser.add_argument("file", nargs="+", help="ELF file to extract")
 
     args = parser.parse_args()
 
     for f in args.file:
+        if args.list_only:
+            funcs = enum_all_funcs(f, args.verbose)
+            print("Functions defined in", f)
+            print("\n".join(["  " + fn for fn in funcs]))
+            continue
         funcs = args.funcs
         if not funcs:
             funcs = enum_all_funcs(f, args.verbose)
@@ -94,5 +106,5 @@ def main():
             extract_func(f, fn, args.verbose)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
